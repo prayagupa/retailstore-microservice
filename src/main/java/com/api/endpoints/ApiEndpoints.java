5a3d5822a186f8e7a6f130a5d9cef90ad531b079
prayagupd <prayag.upd@gmail.com>
2017-03-27 02:04:55 -0700

    add some-properties to be read from appconfig with prodile, but always reads from defaul one

--- src/main/java/com/api/endpoints/ApiEndpoints.java
+++ src/main/java/com/api/endpoints/ApiEndpoints.java
@@ -4,6 +4,7 @@ import com.api.domain.HealthStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -17,8 +18,11 @@ public class ApiEndpoints {
 
     private final AtomicLong counter = new AtomicLong();
 
+    @Value("${some.properties}")
+    String someProperties;
+
     @RequestMapping("/health")
     public HealthStatus health() {
-        return new HealthStatus(counter.incrementAndGet(), "some value", "I'm Running");
+        return new HealthStatus(counter.incrementAndGet(), someProperties, "I'm Running");
     }
 }
