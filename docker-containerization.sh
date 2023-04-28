04d776d3f97dcff612405c2e8ccbf7c59f240498
prayagupa <Prayag.Upd@gmail.com>
2023-04-27 20:21:09 -0700

    update docker container

--- docker-containerization.sh
+++ docker-containerization.sh
@@ -8,6 +8,7 @@ fi
 ./gradlew build
 
 docker build -t retailstore-microservice .
+## runs on bridge network with port mapping localhost:8080=>container:8080
 docker run -it -p 8080:8080 --cpus=1 --memory=768m retailstore-microservice
 
 container_id=$(docker ps -qa --filter ancestor=retailstore-microservice)
